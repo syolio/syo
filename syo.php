21a67d45e53ca2f488f5976a9d8ce66967e93c32
syolio <39416092+syolio@users.noreply.github.com>
2018-05-18 12:44:08 -0500

    Create syo.php

---
+++ syo.php
@@ -0,0 +1,6 @@
+<?php
+$dias=array(“lunes”, “martes”, “miércoles”, “Jueves”, “viernes”, “sábado”, “domingo”)
+Foreach ($dias=$nueva_variable) {	 //NOTA: a la variable $nueva_variable se le asigna el valor de array
+	Echo “hoy es el día” . $nueva_variable;
+}
+?>
